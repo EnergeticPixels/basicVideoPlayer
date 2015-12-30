c581fc1d2a5c3daddedc1f132a7dc34274eaddc5
Anthony Jackman <energeticpixels@gmail.com>
2015-12-30 15:51:43 -0600

    scripting to control clickable progress bar

--- js/player.js
+++ js/player.js
@@ -3,12 +3,13 @@ window.addEventListener('load', function() {
 
     video = document.getElementById('video');
     playButton = document.getElementById('playBtn');
-
+    pbarContainer = document.getElementById('pbar-container');
     pbar = document.getElementById('pbar');
 
     video.load();
     video.addEventListener('canplay', function() {
         playButton.addEventListener('click', playOrPause, false);
+        pbarContainer.addEventListener('click', skip, false);
     }, false);
 
 }, false);
@@ -33,3 +34,13 @@ function updatePlayer() {
         playButton.src = './images/replay.png';
     };
 };
+
+function skip(ev) {
+    var mouseX = ev.pageX - pbarContainer.offsetLeft;
+    // alert(mouseX);
+    var barwidth = window.getComputedStyle(pbarContainer).getPropertyValue('width');
+    barwidth = parseFloat(barwidth.substr(0, barwidth.length - 2));
+    // alert(barwidth);
+    video.currentTime = (mouseX/barwidth) * video.duration;
+    updatePlayer();
+}
