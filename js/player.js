f3e22237c6acda559be205f8e00ce3ff9e1b8bcb
Anthony Jackman <energeticpixels@gmail.com>
2015-12-30 15:26:53 -0600

    basic progress bar scripting

--- js/player.js
+++ js/player.js
@@ -4,6 +4,8 @@ window.addEventListener('load', function() {
     video = document.getElementById('video');
     playButton = document.getElementById('playBtn');
 
+    pbar = document.getElementById('pbar');
+
     video.load();
     video.addEventListener('canplay', function() {
         playButton.addEventListener('click', playOrPause, false);
@@ -15,8 +17,19 @@ function playOrPause() {
     if(video.paused) {
         video.play();
         playButton.src='./images/pause.png';
+        update = setInterval(updatePlayer, 30);
     } else {
         video.pause();
         playButton.src='./images/play.png';
+        window.clearInterval(update);
     }
+};
+
+function updatePlayer() {
+    var percentage = (video.currentTime/video.duration)*100;
+    pbar.style.width = percentage + "%";
+    if(video.ended) {
+        window.clearInterval(update);
+        playButton.src = './images/replay.png';
+    };
 };
